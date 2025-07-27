18ace1b3ed5ec47c80607dbad9ea73867c2ddc27
ShAoN <27mdshaonali@gmail.com>
2025-07-27 21:15:36 +0600

    Now all is working perfectly and updated including filters, sort on logged user role priority and user details activity.

---
+++ app/src/main/java/com/binarybirds/hw258_2/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.binarybirds.hw258_2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DURATION = 3000; // 3 seconds
+    ImageView logoImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        logoImage = findViewById(R.id.logoImage);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in); // or slide_in
+
+        // Start animation
+        logoImage.startAnimation(animation);
+
+        // Move to next activity after delay
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, Sign_In_Activity.class));
+            finish();
+        }, SPLASH_DURATION);
+
+
+    }
+}
